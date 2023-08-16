fb14a748542efbb3b7dc16f48c792721e23343b7
Mohammad-khan-0 <mtk22@njit.edu>
2023-08-15 21:33:48 -0400

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Navbar } from "./components/Navbar";
-import Home from "./components/Home";
-import About from "./components/About";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
 import { Contact } from "./components/Contact";
-import Avatar from "./components/Avatar";
+import { Avatar } from "./components/Avatar";
 
 function App() {
   return (
